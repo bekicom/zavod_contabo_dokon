a86533fa2fc81a5358b47ed340fab8e89fddd894
bekicom <adelkauz6@gmail.com>
2026-02-12 12:20:07 +0500

    s

--- config/db.js
+++ config/db.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 module.exports = async function connectDB() {
+  if (isConnected) return;
+
   try {
     const MONGO_URI = process.env.MONGO_URI;
 
     if (!MONGO_URI) {
-      throw new Error("❌ MONGO_URI .env da topilmadi");
+      throw new Error("MONGO_URI topilmadi");
     }
 
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    });
 
-    console.log("✅ Global MongoDB muvaffaqiyatli ulandi");
+    isConnected = true;
+    console.log("✅ Global MongoDB ulandi");
   } catch (error) {
     console.error("❌ MongoDB ulanish xatosi:", error.message);
-    process.exit(1); // serverni to‘xtatamiz
+    // ❌ process.exit() yo‘q
+    throw error; // faqat throw qilamiz
   }
 };
