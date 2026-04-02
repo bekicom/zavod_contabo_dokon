a7988d33b98badb8369313082694ad0a5b59af38
bekicom <adelkauz6@gmail.com>
2026-04-02 10:52:43 +0500

    server

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-/* =========================
-   DB CONNECT (SAFE FOR VERCEL)
-========================= */
+
 let isConnected = false;
 
 async function ensureDB() {
